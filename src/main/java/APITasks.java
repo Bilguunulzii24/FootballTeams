9e4ab511c52bf982cf3285923fe21d8c92c58dce
anastasiya.ohal <anastasiya.ohal@gmail.com>
2020-03-28 22:39:04 -0500

    getAllMidfielders and getAllMidfieldersFromBrazil

--- src/main/java/APITasks.java
+++ src/main/java/APITasks.java
@@ -1,4 +1,5 @@
 import AllNameOfLigue.CompetitionPojo;
+import Midfielders.AllMidfielders;
 import ToScorers.TopScorerPojo;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -49,10 +50,10 @@ public class APITasks {
         response = httpClient.execute(httpGet);
         objectMapper = new ObjectMapper();
 
-        TeamPojo printTeams = objectMapper.readValue(response.getEntity().getContent(),TeamPojo.class);
+        TeamPojo printTeams = objectMapper.readValue(response.getEntity().getContent(), TeamPojo.class);
 
         List<String> teams = new ArrayList<>();
-        for(int i = 0; i < printTeams.getTeams().size(); i++) {
+        for (int i = 0; i < printTeams.getTeams().size(); i++) {
             teams.add(printTeams.getTeams().get(i).getName());
         }
 
@@ -88,7 +89,30 @@ public class APITasks {
      * Deserialization type: Pojo
      */
     public static List<String> getMidfielders() throws IOException, URISyntaxException {
-        return null;
+
+        httpClient = HttpClientBuilder.create().build();
+        uriBuilder = new URIBuilder();
+        uriBuilder.setScheme("Http").setHost(ConfigReader.getProperty("host")).setPath("/v2/teams/66");
+
+        httpGet = new HttpGet(uriBuilder.build());
+        httpGet.setHeader(ConfigReader.getProperty("contType"), ConfigReader.getProperty("appJson"));
+        httpGet.setHeader("X-Auth-Token", ConfigReader.getProperty("token"));
+        response = httpClient.execute(httpGet);
+        objectMapper = new ObjectMapper();
+
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        Assert.assertTrue(response.getEntity().getContentType().getValue().contains(JSON));
+        AllMidfielders allPlayers = objectMapper.readValue(response.getEntity().getContent(), AllMidfielders.class);
+
+        List<String> midfielders = new ArrayList<>();
+        for (int i = 0; i < allPlayers.getSquad().size(); i++) {
+            if (allPlayers.getSquad().get(i).getPosition().equals("Midfielder")) {
+                midfielders.add(allPlayers.getSquad().get(i).getName());
+            }
+        }
+
+        System.out.println(midfielders);
+        return midfielders;
     }
 
     /*
@@ -97,7 +121,30 @@ public class APITasks {
      * Deserialization type: Pojo
      */
     public static List<String> getMidfielderFromBrazil() throws URISyntaxException, IOException {
-        return null;
+        httpClient = HttpClientBuilder.create().build();
+        uriBuilder = new URIBuilder();
+        uriBuilder.setScheme("Http").setHost(ConfigReader.getProperty("host")).setPath("/v2/teams/66");
+
+        httpGet = new HttpGet(uriBuilder.build());
+        httpGet.setHeader(ConfigReader.getProperty("contType"), ConfigReader.getProperty("appJson"));
+        httpGet.setHeader("X-Auth-Token", ConfigReader.getProperty("token"));
+        response = httpClient.execute(httpGet);
+        objectMapper = new ObjectMapper();
+
+        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
+        Assert.assertTrue(response.getEntity().getContentType().getValue().contains(JSON));
+        AllMidfielders allPlayers = objectMapper.readValue(response.getEntity().getContent(), AllMidfielders.class);
+
+        List<String> midfieldersFromBrazil = new ArrayList<>();
+        for (int i = 0; i < allPlayers.getSquad().size(); i++) {
+            if (allPlayers.getSquad().get(i).getPosition().equals("Midfielder")) {
+                if(allPlayers.getSquad().get(i).getCountryOfBirth().equals("Brazil")||allPlayers.getSquad().get(i).getNationality().equals("Brazil") ){
+                midfieldersFromBrazil.add(allPlayers.getSquad().get(i).getName());
+            }}
+        }
+
+        System.out.println(midfieldersFromBrazil);
+        return midfieldersFromBrazil;
     }
 
     /*
@@ -137,9 +184,9 @@ public class APITasks {
         Assert.assertTrue(response.getEntity().getContentType().getValue().contains(JSON));
 
         //CompetitionPojo allCompetitionsPojo=objectMapper.readValue(response.getEntity().getContent(), CompetitionPojo.class);
-        CompetitionPojo allCompetitionsPojo=objectMapper.readValue(response.getEntity().getContent(),CompetitionPojo.class);
-        List<String> allCompetition=new ArrayList();
-        for (int i=0;i<allCompetitionsPojo.getCompetitions().size();i++){
+        CompetitionPojo allCompetitionsPojo = objectMapper.readValue(response.getEntity().getContent(), CompetitionPojo.class);
+        List<String> allCompetition = new ArrayList();
+        for (int i = 0; i < allCompetitionsPojo.getCompetitions().size(); i++) {
             allCompetition.add(allCompetitionsPojo.getCompetitions().get(i).getName());
             //Playoffs 1/2
         }
